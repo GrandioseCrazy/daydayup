22828c49a7ee2039a81e0da0f7f03701c02efc25
DoubleP <pengp@corp.21cn.com>
2019-11-02 17:45:42 +0800

    Initial commit

---
+++ src/test/java/com/avaj/daydayup/DaydayupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaj.daydayup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaydayupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
